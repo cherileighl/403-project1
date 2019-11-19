1b3811a5a2224039fbe74270ecdf4a60525ebc12
Cherileigh Leavitt <cleavit6@byu.edu>
2019-11-19 11:01:48 -0700

    created model

---
+++ Project1-FoodiesForLife/Models/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1_FoodiesForLife.Models
+{
+    public class Restaurant
+    {
+        
+    }
+}
